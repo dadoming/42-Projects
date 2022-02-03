5fed32adf05cffb85307ea49fc3bcbc66f766415
David Gomes <92876751+dadoming@users.noreply.github.com>
2022-02-03 15:52:59 +0000

    Update libftprintf.h

--- ft_printf/libftprintf.h
+++ ft_printf/libftprintf.h
@@ -10,14 +10,17 @@
 
 int char_counter;
 
+//MAIN EXECUTION
 int ft_printf(char const *format, ...);
 int check_if_in_type (int c);
 int conversion(char c, va_list arg_ptr);
 
+//PRINTERS
 int ft_putchar(char c);
 int ft_putstr(char *str);
 int	ft_putnbr(int n);
 
+//BASE PRINTERS
 int	ft_print_unsigned_and_hexa(unsigned int nbr, char *base, int no);
 int	ft_print_pointeraddress(unsigned long nbr, char *base, int no);
 
