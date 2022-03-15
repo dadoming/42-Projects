b54c46c0c7b537075b0cdadda186e6b090f81661
David Gomes <92876751+dadoming@users.noreply.github.com>
2022-03-15 15:09:30 +0000

    library updated

--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -13,10 +13,25 @@
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-#include "ft_printf.h"
-#include "./libft.h"
 #include <stdio.h>
+#include <stdlib.h>
 
+typedef struct node{
+    int number;
+    struct node* next;
+}   stack;
 
+stack   *add_new_node(int data);
+stack   *init_stacks_string(char **splitted_argv);
+stack   *init_stacks(int argc, char **argv);
+
+int main(int argc, char **argv);
+
+int ft_atoi(const char *str);
+
+char    *ft_substr(char const *s, unsigned int start, size_t len);
+
+size_t  ft_amount_of_splits(char const *s, char c);
+char    **ft_split(char const *s, char c);
 
 #endif
