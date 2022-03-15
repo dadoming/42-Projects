05fa98a5aea8fbf9c4c9c2c4ec0429b754c0fd50
David Gomes <92876751+dadoming@users.noreply.github.com>
2022-03-15 15:11:29 +0000

    Update and rename push_swap_c_lista.c to main.c

--- push_swap/main.c
+++ push_swap/main.c
@@ -11,56 +11,20 @@
 /* ************************************************************************** */
 
 
+#include <stdio.h>
+#include <stdlib.h>
 #include "push_swap.h"
-
-t_list *new_node
-
-typedef t_list stack_l; //(t_list da libft)
-
-void init_stack(argc, argv)
-{
-	int i;
-
-	i = 0;
-	while(argc < i)
-	{
-		if(check_errors(argv[i]))
-		{
-			return( )
-		}
-		a = malloc(sizeof(t_list));
-		a.content = ft_atoi(argv[i]);
-		a.next = head;
-		a.prev = NULL;
-
-		if (head != NULL)
-			head.prev = a;
-		
-		i++;
-
-		/*
-		a.content = ft_lstnew(ft_atoi(argv[i]));
-		a.next = head;
-		*/
-
-}
-
-
-	}
-}
 
 int main(int argc, char **argv)
 {
-	stack_l *a;
-	stack_l *b;
+    stack *a;
+    stack *b;
 
-	if(argc > 1)
-	{
-		a = init_stack(argc, argv); //inicialização do stack a e a sua introdução de valores
-	}
-	
+    a = NULL;
+    if (argc > 1)
+        a = init_stacks(argc, argv);
+    if (argc ) //criar varios meios de para dar sort consoante o valor
 
-	->lstnew to create and alloc node
-	->lstaddfront to add node to beggining of list
-	
+
+    return(0);
 }
