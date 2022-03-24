69174107a35eec37a90aeaabadabc82c6089310e
David Gomes <92876751+dadoming@users.noreply.github.com>
2022-03-24 01:57:41 +0000

    Update moves_push.c

--- push_swap/moves_push.c
+++ push_swap/moves_push.c
@@ -1,58 +1,51 @@
 #include "push_swap"
 
-/*
-apenas dar 'push' e ignorar a perda da lista sendo que nao ha mais nodes
-*/
-static void push(stack **fill_stack, stack **take_stack)
-{
-    stack *head_stack;
-    stack *head_node_to_change;
-
-    head_of_stack = *fill_stack;
-    head_node_to_change = *take_stack;
-    
-}
-
-/*
-para dar 'push' ao node e atribuir o head link ao next node de forma
-a nao perder a lista
-*/
-void push_and_link(stack **fill_stack, stack **take_stack)
-{
-    stack *head_stack;
-    stack *head_node_to_change;
-
-    head_of_stack = *fill_stack;
-    head_node_to_change = *take_stack;
-    
-}
-
-
-
 void pa(stack **stack_a, stack **stack_b)
 {
-    if(ft_lstsize(stack_b) > 1)
+    int holder;
+    stack *new_head_b;
+    
+    if(ft_lstsize(*stack_b) > 0)
     {
-        push_and_link(stack_a, stack_b);
-        write(1, "pa\n", 3);
-    }
-    if(ft_lstsize(stack_b) = 1)
-    {
-        push(stack_a, stack_b);
-        write(1, "pa\n", 3);
+            holder = (*stack_b) -> content;
+            new_head_b = (*stack_b) -> next;
+            ft_lstdelone(*stack_b, NULL);
+            *stack_b = new_head_b;
+            ft_lstadd_front(stack_a, add_new_node(holder));
+            write(1, "pa\n", 3);
+    /*
+            new_head_b = *stack_b;
+            *stack_b = new_head_b -> next;
+            holder = new_head_b -> content;
+            ft_lstdelone(new_head_b, NULL);
+            ft_lstadd_front(stack_a, add_new_node(holder));
+            write(1, "pa\n", 3);
+            */
     }
 }
 
 void pb(stack **stack_a, stack **stack_b)
 {
-    if(ft_lstsize(stack_a) > 1)
+    int holder;
+    stack *new_head_a;
+    
+    if(ft_lstsize(*stack_a) > 0)
     {
-        push_and_link(stack_b, stack_a);
+        
+        holder = (*stack_a) -> content;
+        new_head_a = (*stack_a) -> next;
+        ft_lstdelone(*stack_a, NULL);
+        *stack_a = new_head_a;
+        ft_lstadd_front(stack_b, add_new_node(holder));
         write(1, "pb\n", 3);
-    }
-    if(ft_lstsize(stack_a) = 1)
-    {
-        push(stack_b, stack_a);
-        write(1, "pb\n", 3);
+        
+/* 
+            new_head_a = *stack_a;
+            *stack_a = new_head_a -> next;
+            holder = new_head_a -> content;
+            ft_lstdelone(new_head_a, NULL);
+            ft_lstadd_front(stack_b, add_new_node(holder));
+            write(1, "pb\n", 3);
+  */
     }
 }
