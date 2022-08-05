8b030150d23e1a335a6fc027b8745995052f9269
Davz <davidmdg98@hotmail.com>
2022-08-05 19:10:20 +0100

    Started implementation of Radix sort

--- push_swap/sort_big.c
+++ push_swap/sort_big.c
@@ -12,57 +12,44 @@
 
 #include "push_swap.h"
 
+void radix(t_list **a, t_list **b)
+{
+    int size = ft_lstsize(*a);
+    int max_num = size - 1; // the biggest number in stack a will be size - 1
+    int max_bits = 0;
+
+    // Loops through the operation until reaches condition to get the
+    // maximum number of bits that need to be parsed right until all 00000000
+    // to get the amount of times that the next loop will loop
+    // For example: 13>>0 = 13
+    //              13>>1 = 6
+    //              13>>2 = 3
+    //              13>>3 = 1
+    //              13>>4 = 0
+    while (max_num >> max_bits != 0)
+        ++max_bits;
+    int i = 0;
+    int j = 0;
+    while (i < max_bits)
+    {
+        while (++j < size)
+        {
+            int num = (*a)->content;
+            if(((num >> i) & 1) == 1)
+                ra(a);
+            else
+                pb(a, b);
+        }
+        j = 0;
+        i++;
+    }
+    while(b)
+    {
+        pa(a, b);
+    }
+}
+
 void sort_big(t_list **a, t_list **b)
 {
-    head of list = *a;
-    int j = 0; //posicao do elemento mais pequeno
-    int k = 0; //posicao do segundo elemento mais pequeno
-    int hold_value_a = 0;
-    int hold_value_b = 0;
-    
-    int i = 0;
-    while(ft_lstsize(a) >= ft_lstsize(b))
-    {
-        if(*a -> content < *a -> next -> content)
-        {
-            hold_value_a = *a -> content;
-            j = i;
-        }
-        i++;
-        *a = *a -> next;
-    }
-    
-    (a) = head
-
-    i = 0;
-    if(hold_value_a > hold_value_b)
-    {
-        if(j > ft_lstsize(a) / 2)
-        {
-            while (a-> content)
-            {
-                rra(a);
-                if(j = i)
-                {
-                    pb();
-                    break;
-                }
-                i++
-            }
-        }
-        else
-        {
-            while(a -> content)
-            {
-                
-            }
-        }
-    }
-    if(hold_value_b > hold_value_a)
-    {
-        while (k)
-        {
-            
-        }
-    }
+    radix(a, b);
 }
